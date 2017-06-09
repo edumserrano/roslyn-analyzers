d9ce5df834604aa7696219af5a4f6f23faae68a9
Eduardo Serrano <eduardomserrano@outlook.com>
2017-06-09 16:13:36 +0100

    todo stuff

--- Source/RoslynAnalyzers/Analyzers/Todo.cs
+++ Source/RoslynAnalyzers/Analyzers/Todo.cs
@@ -12,6 +12,15 @@ namespace Analyzers
     //meaning the one that was reported on the anaylizer by context.ReportDiagnostic(Diagnostic.Create(Rule, defaultLabel.GetLocation()));
 #warning also create a wiki with how to configure analysers for project and solution (there is something on roslynator docs i think)
 
+    //Google   "visual studio 2017" reset visual studio experimental instance
+    // https://www.codeproject.com/Tips/832362/Resetting-the-Visual-Studio-Experimental-Instance
+    // https://msdn.microsoft.com/en-us/library/bb166507.aspx
+    //
+    // C:\Users\eduar\AppData\Local\Microsoft\VisualStudio
+    // C:\Program Files(x86)\Microsoft Visual Studio\2017\Enterprise\VSSDK\VisualStudioIntegration\Tools\Bin>
+    // CreateExpInstance.exe /Reset /VSInstance=15.0 /RootSuffix=_9e84fa4fRoslyn
+
+
     class Todo
     {
     }
