2c5aad15740e8c0622e3299de04e46f69adcbf6d
Eduardo Serrano <eduardomserrano@outlook.com>
2017-06-29 13:26:34 +0100

    tests

---
+++ source/RoslynAnalyzers/Analyzers.Tests/UnitTest2.cs
@@ -0,0 +1,44 @@
+﻿using Analyzers.CodeAnalysis.Classes.SetClassAsSealedIfPossible;
+using Analyzers.Tests._TestEnvironment.Base;
+using Xunit;
+
+namespace Analyzers.Tests
+{
+    public class UnitTest2
+        : CSharpCodeFixProviderTest<SetClassAsSealedIfPossibleDiagnosticAnalyzer, SetClassAsSealedIfPossibleCodeFix>
+    {
+        //Diagnostic and CodeFix both triggered and checked for
+        [Fact]
+        public void TestMethod2()
+        {
+            var test = @"
+        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Text;
+        using System.Threading.Tasks;
+
+        namespace Analyzers.Tests
+        {
+            public class MyClass
+            {
+            }
+        }";
+
+            var fixtest = @"
+        using System;
+        using System.Collections.Generic;
+        using System.Linq;
+        using System.Text;
+        using System.Threading.Tasks;
+
+        namespace Analyzers.Tests
+        {
+            public sealed class MyClass
+            {
+            }
+        }";
+            VerifyFix(test, fixtest);
+        }
+    }
+}
---
+++ source/RoslynAnalyzers/Analyzers.Tests/_TestEnvironment/Base/CSharpCodeFixProviderTest.cs
@@ -0,0 +1,42 @@
+﻿using Analyzers.Tests._TestEnvironment.Roslyn;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Analyzers.Tests._TestEnvironment.Base
+{
+    public abstract class CSharpCodeFixProviderTest<TDiagnosticAnalyzer, TCodeFixProvider>
+        where TDiagnosticAnalyzer : DiagnosticAnalyzer, new()
+        where TCodeFixProvider : CodeFixProvider, new()
+    {
+        private readonly CodeFixProvider _codeFixProvider;
+        private readonly TDiagnosticAnalyzer _diagnosticAnalyzer;
+
+        protected CSharpCodeFixProviderTest()
+        {
+            _diagnosticAnalyzer = new TDiagnosticAnalyzer();
+            _codeFixProvider = new TCodeFixProvider();
+        }
+        /// <summary>
+        /// Called to test a C# codefix when applied on the inputted string as a source
+        /// </summary>
+        /// <param name="oldSource">A class in the form of a string before the CodeFix was applied to it</param>
+        /// <param name="newSource">A class in the form of a string after the CodeFix was applied to it</param>
+        /// <param name="codeFixIndex">Index determining which codefix to apply if there are multiple</param>
+        /// <param name="allowNewCompilerDiagnostics">A bool controlling whether or not the test will fail if the CodeFix introduces other warnings after being applied</param>
+        public void VerifyFix(
+            string oldSource,
+            string newSource,
+            int? codeFixIndex = null,
+            bool allowNewCompilerDiagnostics = false)
+        {
+            _codeFixProvider.VerifyFix(
+                language: LanguageNames.CSharp,
+                analyzer: _diagnosticAnalyzer,
+                oldSource: oldSource,
+                newSource: newSource,
+                codeFixIndex: codeFixIndex,
+                allowNewCompilerDiagnostics: allowNewCompilerDiagnostics);
+        }
+    }
+}
